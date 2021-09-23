124da92086cc01a6626f79933938f2fc6fd77670
Dionesis <dionesis@protonmail.com>
2021-09-23 02:15:21 +0200

    fetch

--- js/api/firebase.js
+++ js/api/firebase.js
@@ -157,21 +157,21 @@ fbHelper.getBasketCount = () => {
 fbHelper.getBasketCount();
 
 fbHelper.submitOrder = (nom, email, tel) => {
-  axios
-    .post(
-      "https://formablocs-api.herokuapp.com/create-checkout-session",
-
-      {
-        articles: JSON.parse(localStorage.getItem("basket")),
-        date: firebase.firestore.FieldValue.serverTimestamp(),
-        email,
-        nom,
-        telephone: tel,
-        total: localStorage.getItem("amount"),
-      }
-    )
-    .then((response) => {
-      if (response) {
+  fetch("https://formablocs-api.herokuapp.com/create-checkout-session", {
+    method: "POST",
+    mode: "cors",
+    body: {
+      articles: JSON.parse(localStorage.getItem("basket")),
+      date: firebase.firestore.FieldValue.serverTimestamp(),
+      email,
+      nom,
+      telephone: tel,
+      total: localStorage.getItem("amount"),
+    },
+  })
+    .then((response) => response.json())
+    .then((resp) => {
+      if (resp) {
         db.collection("orders")
           .add({
             articles: JSON.parse(localStorage.getItem("basket")),
